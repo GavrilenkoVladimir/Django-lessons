0bdedd48605f9a59aacc0cc60613c77dd5a70317
GavrilenkoVladimir <vladimirvgavrilenko@gmail.com>
2026-01-09 07:51:29 +0200

    Created login and logout templates and views

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from django.utils.http import parse_http_date
 
-from catalog.views import index, LiteraryFormatView, BookListView, AuthorListView, BookDetailView,test_session_view
+from catalog.views import (
+    index,
+    LiteraryFormatView,
+    BookListView,
+    AuthorListView,
+    BookDetailView,
+    test_session_view,
+    AuthorDetailView
+)
 
 urlpatterns = [
     path("", index, name="index"),
@@ -9,6 +17,7 @@ urlpatterns = [
     path("books/", BookListView.as_view(), name="book-list"),
     path("books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),
     path("authors/", AuthorListView.as_view(), name="author-list"),
+    path("authtors/<int:pk>", AuthorDetailView.as_view(), name="author-detail"),
     path("test-session/", test_session_view, name="test-session"),
 
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -10,10 +10,13 @@ def index(request: HttpRequest) -> HttpResponse:
     num_books = Book.objects.count()
     num_authors = Author.objects.count()
     num_literary_formats = LiteraryFormat.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
     context = {
         "num_books": num_books,
         "num_authors": num_authors,
         "num_literary_formats": num_literary_formats,
+        "num_visits": request.session["num_visits"],
     }
     return render(request, "catalog/index.html", context=context)
 
@@ -22,6 +25,7 @@ class LiteraryFormatView(generic.ListView):
     template_name = "catalog/literary_format_list.html"
     context_object_name = "literary_format_list"
     queryset = LiteraryFormat.objects.all()
+    paginate_by = 2
 
 
 class BookListView(generic.ListView):
@@ -30,11 +34,18 @@ class BookListView(generic.ListView):
     paginate_by = 2
 
 
-class AuthorListView(generic.ListView):
-    model = Author
-
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    queryset = Author.objects.prefetch_related("books")
 
 
 def test_session_view(request):
@@ -42,4 +53,6 @@ def test_session_view(request):
         "<h1>Test Session</h1>"
         f"<h4>Session data: {request.session['book']}</h4>"
     )
+
+
 
